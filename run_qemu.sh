6caae4a2eecbe9a76e934e1238cc717643e4c3d5
Matuesz <bieganski.m@wp.pl>
2024-07-04 23:12:20 +0200

    mon:stdio serial works

--- run_qemu.sh
+++ run_qemu.sh
@@ -8,6 +8,8 @@ QEMU=/home/mateusz/tb/github/qemu/build/qemu-system-aarch64
 KERNEL=$SCRIPT_DIR/u-boot.bin
 DTB=$SCRIPT_DIR/arch/arm/dts/bcm2711-rpi-4-b.dtb
 
+DTB=$SCRIPT_DIR/REAL.bcm2711-rpi-4-b.dtb
+
 set -x
 
 sudo $QEMU \
@@ -17,6 +19,6 @@ sudo $QEMU \
 	-serial mon:stdio \
 	-dtb $DTB \
 	-kernel $KERNEL \
-
-# -nographic \
+	-nographic \
+	
 # -monitor telnet:localhost:4322,server,nowait 
